30a75489f128caee0b2d4ee9c4b502e4ede902c4
MuhammadAkmalHamidi <132432072+MuhammadAkmalHamidi@users.noreply.github.com>
2023-07-26 12:25:03 +0700

    dependencies rollback (#8)
    * package errors
    * dependencies rollback
    
    ---------
    
    Co-authored-by: MuhammadAkmalHamidi <hamidiakmal139@gmail.com>
    Co-authored-by: Amadeo Marli <132928847+asc33nzio@users.noreply.github.com>

--- projects/client/src/App.jsx
+++ projects/client/src/App.jsx
@@ -1,7 +1,16 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Login } from "./pages/login";
+import { Forgot } from "./pages/forgot";
+import { ErrorPage } from "./pages/error";
 import { Navbar } from "./components/navbar";
 import { Cashier } from "./pages/cashier";
 
+const router = createBrowserRouter([
+  { path: "/", element: <Login />, errorElement: <ErrorPage /> },
+  { path: "/forgot", element: <Forgot />, },
+  { path: "/cashier", element : <Cashier />}
+
+]);
 
 function App() {
   return (
