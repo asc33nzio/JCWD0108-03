8a22e600a651d24e1864a2a1784aed511d52cb37
asc33nzio <asc33nzio.dev@gmail.com>
2023-07-26 12:12:13 +0700

    Revert "Auth (#7)"
    This reverts commit 4ff4b9b799cd93aed4669fec6b3c1fc0d5195819.

--- projects/client/src/App.jsx
+++ projects/client/src/App.jsx
@@ -1,22 +1,14 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Login } from "./pages/login";
-import { Forgot } from "./pages/forgot";
-import { ErrorPage } from "./pages/Error";
+import { ChakraProvider } from "@chakra-ui/react";
 import { Navbar } from "./components/navbar";
 import { Cashier } from "./pages/cashier";
 
-const router = createBrowserRouter([
-  { path: "/", element: <Login />, errorElement: <ErrorPage /> },
-  { path: "/forgot", element: <Forgot />, },
-
-]);
 
 function App() {
   return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
-  );
+  <ChakraProvider>
+    <Cashier />
+  </ChakraProvider>
+  )
 }
 
 export default App;
