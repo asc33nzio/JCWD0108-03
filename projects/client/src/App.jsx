4ff4b9b799cd93aed4669fec6b3c1fc0d5195819
alvianaryobimo <117703346+alvianaryobimo@users.noreply.github.com>
2023-07-26 11:39:38 +0700

    Auth (#7)
    Login, forget, 404 Page

--- projects/client/src/App.jsx
+++ projects/client/src/App.jsx
@@ -1,14 +1,22 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Login } from "./pages/login";
+import { Forgot } from "./pages/forgot";
+import { ErrorPage } from "./pages/Error";
 import { Navbar } from "./components/navbar";
 import { Cashier } from "./pages/cashier";
 
+const router = createBrowserRouter([
+  { path: "/", element: <Login />, errorElement: <ErrorPage /> },
+  { path: "/forgot", element: <Forgot />, },
+
+]);
 
 function App() {
   return (
-  <ChakraProvider>
-    <Cashier />
-  </ChakraProvider>
-  )
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
