d015784a97110c70728aff9ba1d86b1a6605c263
asc33nzio <asc33nzio.dev@gmail.com>
2023-07-27 14:19:49 +0700

    Initialized users model

---
+++ projects/server/src/models/Users.js
@@ -0,0 +1,39 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    class Users extends Model {
+
+    }
+
+    Users.init({
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        isAdmin: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+        avatar: {
+            type: DataTypes.STRING,
+            defaultValue: null
+        },
+        isSuspended: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
+    }, {
+        sequelize,
+        modelName: 'Users',
+    });
+    return Users;
+};
