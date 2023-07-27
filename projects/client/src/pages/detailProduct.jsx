1dc4e9dc1e384bdadfab6df5f689f82481fd05e8
MuhammadAkmalHamidi <hamidiakmal139@gmail.com>
2023-07-28 02:02:10 +0700

    create detail product

--- projects/client/src/pages/detailProduct.jsx
+++ projects/client/src/pages/detailProduct.jsx
@@ -1,12 +1,15 @@
 import { Box, Button, Flex, Image } from "@chakra-ui/react";
 import { Navbar } from "../components/navbar";
 import Axios from "axios";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { Cart } from "../components/cart";
+import { Back } from "../components/back";
+
 
 export const DetailProduct = () => {
     const params = useParams()
+    const navigate = useNavigate()
 
     const [product, setProduct] = useState([])
 
@@ -19,6 +22,8 @@ export const DetailProduct = () => {
         }
     }
 
+
+
     useEffect(() => {
         dataProduct()
     }, [])
@@ -27,24 +32,27 @@ export const DetailProduct = () => {
     return (
         <Box>
             <Box><Navbar /></Box>
-            <Flex justifyContent={"center"} pt={"100px"} px={"150px"}>
-                <Box>
+            <Flex>
+                <Back />
+                <Flex justifyContent={"center"} pt={"100px"} px={"150px"}>
                     <Box>
-                        <Image borderRadius={"5px"} boxShadow={"0px 0px 10px black"} w={"400px"} h={"350px"} src={`http://localhost:8000/api/products/image/${product?.imgURL}`} />
-                    </Box>
-                    <Flex w={"400px"} h={"90px"} justifyContent={"space-between"} mt={"5px"}>
-                        <Box mb={"110px"} alignContent={"center"}>
-                            <Box textShadow={"2px 2px 3px gray"} fontSize={"50px"} fontWeight={"bold"} mb={"20px"}> {product.productName} </Box>
+                        <Box>
+                            <Image borderRadius={"5px"} boxShadow={"0px 0px 10px black"} w={"400px"} h={"350px"} src={`http://localhost:8000/api/products/image/${product?.imgURL}`} />
                         </Box>
-                        <Flex alignItems={"center"}>
-                            <Button bgColor={"yellow.400"} color={"white"}>Add To Cart</Button>
+                        <Flex w={"400px"} h={"90px"} justifyContent={"space-between"} mt={"5px"}>
+                            <Box mb={"110px"} alignContent={"center"}>
+                                <Box textShadow={"2px 2px 3px gray"} fontSize={"50px"} fontWeight={"bold"} mb={"20px"}> {product.productName} </Box>
+                            </Box>
+                            <Flex alignItems={"center"}>
+                                <Button bgColor={"yellow.400"} color={"white"}>Add To Cart</Button>
+                            </Flex>
                         </Flex>
-                    </Flex>
-                    <Box textAlign={"justify"} w={"400px"}> {product.description} </Box>
-                </Box>
-                <Box>
-                    <Cart />
-                </Box>
+                        <Box textAlign={"justify"} w={"400px"}> {product.description} </Box>
+                    </Box>
+                    <Box>
+                        <Cart />
+                    </Box>
+                </Flex>
             </Flex>
 
         </Box>
