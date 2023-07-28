41b7f84090b78cf25cc686569e4edf98c7f23279
asc33nzio <asc33nzio.dev@gmail.com>
2023-07-28 15:18:22 +0700

    Hotfixes

--- projects/client/src/index.js
+++ projects/client/src/index.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeProvider, extendTheme } from '@chakra-ui/react';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+
+const colorModeConfig = {
+  initialColorMode: "light",
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({ config: { initialColorMode: colorModeConfig.initialColorMode } });
+
+const DarkModeWrapper = ({ children }) => {
+  React.useEffect(() => {
+    localStorage.setItem('chakra-ui-color-mode', 'light');
+    localStorage.removeItem('chakra-ui-color-mode');
+    localStorage.setItem('chakra-ui-color-mode', 'light');
+  });
+  return <>{children}</>;
+};
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ChakraProvider>
+    <DarkModeWrapper>
+      <ColorModeProvider options={colorModeConfig}>
+        <Provider store={store}>
+          <ChakraProvider theme={theme}>
 
-        <App />
-      </ChakraProvider>
-    </Provider>
+            <App />
+          </ChakraProvider>
+        </Provider>
+      </ColorModeProvider>
+    </DarkModeWrapper>
   </React.StrictMode>
 );
