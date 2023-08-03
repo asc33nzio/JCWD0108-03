b636538f8f5320c7e73ab0929c443833d2f71589
MuhammadAkmalHamidi <hamidiakmal193@gmail.com>
2023-08-04 00:25:22 +0700

    edit detail product

--- projects/client/src/pages/detailProduct.jsx
+++ projects/client/src/pages/detailProduct.jsx
@@ -82,8 +82,8 @@ export const DetailProduct = () => {
                         </Box>
                         <Flex w={{ base: "250px", sm: "250px", md: "300px", lg: "400px" }} h={{ base: "20px", sm: "40px", md: "60px" }} mb={{ base: "5px", sm: "30px", md: "20px" }} pb={{ base: "30px", sm: "45px" }} lineHeight={{ base: "20px", sm: "40px" }} justifyContent={"space-between"} mt={{ base: "10px", sm: "15px" }}>
                             <Box mb={"110px"} alignContent={"center"} mr={{ base: "5px" }}>
-                                <Box pb={"20px"} borderBottom={"2px solid gray"} w={{ base: "250px", sm: "250px", md: "300px", lg: "400px" }} textShadow={"2px 2px 2px gray"} fontSize={{ base: "25px", sm: "35px", md: "45px" }} color={"gray.700"} fontFamily={"sans-serif"} fontWeight={"bold"} > {product.productName} </Box>
-                                <Flex fontSize={"20px"} color={"gray.600"} w={{ base: "250px", sm: "250px", md: "300px", lg: "400px" }} pb={"10px"} borderBottom={"2px solid gray"} fontFamily={"sans-serif"} ><Box fontWeight={"bold"} fontSize={{ base: "15px", sm: "17px", md: "24px" }} >Stock Product :</Box> <Box ml={"5px"} fontSize={{ base: "15px", sm: "17px", md: "24px" }} >{product.stock}</Box> </Flex>
+                                <Box pb={"20px"} borderBottom={"2px solid gray"} textShadow={"2px 2px 2px gray"} fontSize={{ base: "25px", sm: "35px", md: "45px" }} color={"gray.700"} fontFamily={"sans-serif"} fontWeight={"bold"} > {product.productName} </Box>
+                                <Flex fontSize={"20px"} color={"gray.600"} pb={"10px"} borderBottom={"2px solid gray"} fontFamily={"sans-serif"} ><Box fontWeight={"bold"} fontSize={{ base: "15px", sm: "17px", md: "24px" }} >Stock Product :</Box> <Box ml={"5px"} fontSize={{ base: "15px", sm: "17px", md: "24px" }} >{product.stock}</Box> </Flex>
                             </Box>
                             <Flex>
                                 {data ? (<Flex alignItems={"center"} gap={{ base: "5px" }} mt={{ base: "10px", sm: "25px" }}>
@@ -101,7 +101,7 @@ export const DetailProduct = () => {
                             </Box>
                         </Box>
                     </Box>
-                    <Box>
+                    <Box ml={"30px"}>
                         <Cart cartItems={cartItems} setCartItems={setCartItems} updatedQuantities={updatedQuantities} setUpdatedQuantities={setUpdatedQuantities} />
                     </Box>
                 </Flex>
