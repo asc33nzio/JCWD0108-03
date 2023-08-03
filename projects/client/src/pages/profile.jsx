990be4f4c5bc132c1d06d88e90bd6a4df6ac580f
alvianaryobimo <117703346+alvianaryobimo@users.noreply.github.com>
2023-08-03 21:36:09 +0700

    toast for update profile (#49)
    * profile page toast update

--- projects/client/src/pages/profile.jsx
+++ projects/client/src/pages/profile.jsx
@@ -1,8 +1,8 @@
 import Axios from 'axios';
 import * as Yup from "yup";
 import { Field, Form, Formik } from 'formik';
-import { Avatar, Box, Button, Flex, Heading, Input, Text, useToast } from "@chakra-ui/react"
-import { Navbar } from "../components/navbar"
+import { Avatar, Box, Button, Flex, Heading, Input, Text, useToast } from "@chakra-ui/react";
+import { Navbar } from "../components/navbar";
 import { useSelector } from "react-redux";
 import { useState } from "react";
 import { useNavigate } from 'react-router';
@@ -14,15 +14,16 @@ export const Profile = ({ id }) => {
     const token = localStorage.getItem("token");
     const data = useSelector((state) => state.user.value);
     const [file, setFile] = useState(null);
-    const Formschema = Yup.object().shape(({
+    const Formschema = Yup.object().shape({
         avatar: Yup.string()
             .required("Add image"),
-    }));
+    });
+
     const handleCreate = async (value) => {
         try {
             const data = new FormData();
             data.append("avatar", file);
-            await Axios.patch(`http://localhost:8000/api/users/updateprofile`, data, {
+            const response = await Axios.patch(`http://localhost:8000/api/users/updateprofile`, data, {
                 headers: { Authorization: `Bearer ${token}` },
                 "content-Type": "Multiple/form-data"
             });
@@ -30,27 +31,37 @@ export const Profile = ({ id }) => {
                 title: "Profile Updated!",
                 description: "Your Profile Updated!",
                 status: 'success',
-                duration: 1500,
+                duration: 1000,
                 isClosable: true,
                 position: "top"
             });
-            window.location.reload();
-            navigate("/profile");
+            console.log(response);
+            setTimeout(() => {
+                window.location.reload();
+                navigate("/profile");
+            }, 1000);
         } catch (err) {
             console.log(err);
+            toast({
+                title: "Failed to Change your Photo Profile!",
+                description: "File should be less than 1MB or File is not supported",
+                status: 'error',
+                duration: 3000,
+                isClosable: true,
+                position: "top"
+            });
         }
-    }
+    };
+
     return (
         <>
-            <Flex><Navbar /></Flex>
-            <Flex justifyContent={"center"}>
-                <Flex justifyContent={"center"} mt={"135px"} borderRadius={"10px"}
-                    boxShadow='0px 0px 6px black' bgGradient={"linear(yellow.500,#FFC900)"} w={"700px"} h={"450px"}>
-                    <Flex mt={"48px"} borderRadius={"10px"} justifyContent={"center"}
-                        boxShadow='0px 0px 6px black' bg={"white"} w={"590px"} h={"350px"}>
-                        <Box mr={"45px"}>
-                            <Flex mt={"20px"} justifyContent={"center"} >
-                                <Avatar src={`http://localhost:8000/avatars/${data.avatar}`} mt={"25px"} boxShadow={"0px 0px 4px black"} bg={"grey"} w={"230px"} h={"230px"} />
+            <Navbar />
+            <Flex justifyContent="center">
+                <Flex justifyContent="center" mt={{ base: "50px", md: "135px" }} borderRadius="10px" boxShadow="0px 0px 6px black" bgGradient="linear(yellow.500,#FFC900)" w={{ base: "90%", md: "700px" }} h="450px">
+                    <Flex mt="48px" borderRadius="10px" justifyContent="center" boxShadow="0px 0px 6px black" bg="white" w="90%" h="350px">
+                        <Box mr={{ base: 0, md: "45px" }}>
+                            <Flex mt="20px" justifyContent="center" >
+                                <Avatar src={`http://localhost:8000/avatars/${data.avatar}`} mt="25px" boxShadow="0px 0px 4px black" bg="grey" w={{ base: "100px", md: "230px" }} h={{ base: "100px", md: "230px" }} />
                             </Flex>
                             <Formik
                                 initialValues={{ avatar: null }}
@@ -60,14 +71,13 @@ export const Profile = ({ id }) => {
                                 }}>
                                 {(props) => {
                                     return (
-                                        <>
-                                            <Flex position={"relative"} as={Form}
-                                                justifyContent={"center"}>
-                                                <Field name="avatar" >
+                                        <Form>
+                                            <Flex position="relative" justifyContent="center">
+                                                <Field name="avatar">
                                                     {({ field }) => (
                                                         <>
-                                                            <label htmlFor="customFileInput" >
-                                                                <Text h={"25px"} mt={"-28px"} ml={"80px"} position={"absolute"} fontSize={"12px"} as="span" bg="gray.500" color="white" px={"7px"} pb={"5px"} pt={"2px"} borderRadius="md" cursor="pointer" _hover={{ bg: 'green.600' }}>
+                                                            <label htmlFor="customFileInput">
+                                                                <Text h="25px" mt="-28px" ml="80px" position="absolute" fontSize="12px" as="span" bg="gray.500" color="white" px="7px" pb="5px" pt="2px" borderRadius="md" cursor="pointer" _hover={{ bg: 'green.600' }}>
                                                                     <AddIcon />
                                                                 </Text>
                                                             </label>
@@ -77,51 +87,44 @@ export const Profile = ({ id }) => {
                                                                     field.onChange(e);
                                                                     setFile(e.target.files[0]);
                                                                 }}
-                                                                placeholder='Photo' name='avatar'
+                                                                placeholder="Photo"
+                                                                name="avatar"
                                                                 type="file"
                                                                 id="customFileInput"
-                                                                style={{ display: 'none' }} />
+                                                                style={{ display: 'none' }}
+                                                            />
                                                         </>
                                                     )}
                                                 </Field>
-                                                <Flex justifyContent={'center'}>
-                                                    <Button isDisabled={!props.dirty} type='submit' w={"80px"} h={"30px"} lineHeight={"30px"} mt={"20px"} cursor="pointer" _hover={{ bg: 'green.600' }} fontSize={"17px"} bg="yellow.500" color="white">Save</Button>
+                                                <Flex justifyContent="center">
+                                                    <Button isDisabled={!props.dirty} type="submit" w="80px" h="30px" lineHeight="30px" mt="20px" cursor="pointer" _hover={{ bg: 'green.600' }} fontSize="17px" bg="yellow.500" color="white">Save</Button>
                                                 </Flex>
                                             </Flex>
-                                        </>
+                                        </Form>
                                     );
                                 }}
                             </Formik>
                         </Box>
-                        <Box mt={"93px"}>
-                            <Flex mt={"15px"} justifyContent={"center"}>
-                                <Heading fontSize={"35px"} fontWeight={"extrabold"}>
+                        <Box mt={{ base: "20px", md: "93px" }}>
+                            <Flex mt="15px" justifyContent="center">
+                                <Heading fontSize="35px" fontWeight="extrabold">
                                     {data.username}
                                 </Heading>
                             </Flex>
-                            <Flex fontSize={"17px"} mt={"5px"} justifyContent={"center"}>
-                                <Text fontFamily={"monospace"}>
+                            <Flex fontSize="17px" mt="5px" justifyContent="center">
+                                <Text fontFamily="monospace">
                                     {data.email}
                                 </Text>
                             </Flex>
-                            {data.isAdmin ? (
-                                <Flex mt={"10px"} justifyContent={"center"}>
-                                    <Text textAlign={"center"} boxShadow={"0px 0px 10px grey"} justifyContent={"center"} bgColor={"green.300"} h={"30px"} w={"100px"} lineHeight={"30px"} color={"white"} borderRadius={"5px"} fontFamily={"monospace"}>
-                                        Admin
-                                    </Text>
-                                </Flex>
-                            ) : (
-                                <Flex mt={"10px"} justifyContent={"center"}>
-                                    <Text textAlign={"center"} boxShadow={"0px 0px 10px grey"} justifyContent={"center"} bgColor={"green.300"} h={"30px"} w={"100px"} lineHeight={"30px"} color={"white"} borderRadius={"5px"} fontFamily={"monospace"}>
-                                        Cashier
-                                    </Text>
-                                </Flex>
-                            )}
+                            <Flex mt="10px" justifyContent="center">
+                                <Text textAlign="center" boxShadow="0px 0px 10px grey" justifyContent="center" bgColor="green.300" h="30px" w="100px" lineHeight="30px" color="white" borderRadius="5px" fontFamily="monospace">
+                                    {data.isAdmin ? "Admin" : "Cashier"}
+                                </Text>
+                            </Flex>
                         </Box>
                     </Flex>
                 </Flex>
             </Flex>
-
         </>
-    )
-}
+    );
+};
