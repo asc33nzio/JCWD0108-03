953cfdf3fde17648e64488c96ff33d6daac33f61
asc33nzio <asc33nzio.dev@gmail.com>
2023-08-04 01:25:34 +0700

    Code and assets clean, staging for aplha release

--- projects/client/src/pages/salesGraph.jsx
+++ projects/client/src/pages/salesGraph.jsx
@@ -1,12 +1,14 @@
 import Axios from "axios";
-import { useEffect, useState } from "react";
+import Chart from "chart.js/auto"; 
+import moment from "moment"; 
+import { useEffect, useState, useRef } from "react";
 import { Box } from "@chakra-ui/react";
 import { Navbar } from "../components/navbar";
-import { Line } from "react-chartjs-2";
+import 'chartjs-adapter-moment'; 
 
 export const SalesGraph = () => {
     const [salesData, setSalesData] = useState([]);
-    const [chartInstance, setChartInstance] = useState(null); // Define chartInstance outside of useEffect
+    const chartInstanceRef = useRef(null);
 
     useEffect(() => {
         Axios.get("http://localhost:8000/api/transactions/sales")
@@ -21,82 +23,93 @@ export const SalesGraph = () => {
             .catch((error) => {
                 console.error("Error fetching sales data:", error);
             });
+    }, []);
 
-        // Cleanup function to destroy the Chart.js instance when the component unmounts
-        return () => {
-            if (chartInstance) {
-                chartInstance.destroy();
-            }
+    useEffect(() => {
+        if (chartInstanceRef.current) {
+            chartInstanceRef.current.destroy();
+        }
+
+        const formatSaleData = (data) => {
+            const groupedData = {};
+            data.forEach((record) => {
+                const date = record.Transaction.txTime.split("T")[0]; 
+                if (!groupedData[date]) {
+                    groupedData[date] = 0;
+                }
+                groupedData[date] += record.Product.totalAmount;
+            });
+
+            const dates = Object.keys(groupedData);
+            const saleAmounts = Object.values(groupedData);
+            return { dates, saleAmounts };
         };
-    }, []); // Pass an empty dependency array to run the effect only once
 
-    const formatSaleData = (data) => {
-        // Group sales data by day and calculate total sale nominal amount for each day
-        const groupedData = {};
-        data.forEach((record) => {
-            const date = record.Transaction.txTime.split("T")[0]; // Extract date from txTime
-            if (!groupedData[date]) {
-                groupedData[date] = 0;
-            }
-            groupedData[date] += record.Product.totalAmount;
-        });
+        const salesChartData = formatSaleData(salesData);
 
-        // Convert the grouped data into arrays for chart.js
-        const dates = Object.keys(groupedData);
-        const saleAmounts = Object.values(groupedData);
-        return { dates, saleAmounts };
-    };
+        if (salesChartData.dates.length > 0) {
+            const chartData = {
+                labels: salesChartData.dates,
+                datasets: [
+                    {
+                        label: "Sales",
+                        data: salesChartData.saleAmounts,
+                        fill: false,
+                        borderColor: "rgba(75,192,192,1)",
+                        tension: 0.4,
+                    },
+                ],
+            };
 
-    const salesChartData = formatSaleData(salesData);
-
-    const chartData = {
-        labels: salesChartData.dates,
-        datasets: [
-            {
-                label: "Sales",
-                data: salesChartData.saleAmounts,
-                fill: false,
-                borderColor: "rgba(75,192,192,1)",
-                tension: 0.4,
-            },
-        ],
-    };
-
-    const chartOptions = {
-        scales: {
-            x: {
-                type: "time",
-                time: {
-                    parser: "YYYY-MM-DD",
-                    tooltipFormat: "ll",
-                    unit: "day",
-                    displayFormats: {
-                        day: "YYYY-MM-DD",
+            const chartOptions = {
+                scales: {
+                    x: {
+                        type: "time",
+                        time: {
+                            parser: "YYYY-MM-DD",
+                            tooltipFormat: "ll",
+                            unit: "day",
+                            displayFormats: {
+                                day: "YYYY-MM-DD",
+                            },
+                        },
+                        title: {
+                            display: true,
+                            text: "Date",
+                        },
+                        adapters: {
+                            date: moment, // Use the moment date adapter
+                        },
+                    },
+                    y: {
+                        title: {
+                            display: true,
+                            text: "Sales (Nominal Amount)",
+                        },
                     },
                 },
-                title: {
-                    display: true,
-                    text: "Date",
-                },
-            },
-            y: {
-                title: {
-                    display: true,
-                    text: "Sales (Nominal Amount)",
-                },
-            },
-        },
-    };
+            };
+
+            const ctx = document.getElementById('salesGraph').getContext('2d');
+            chartInstanceRef.current = new Chart(ctx, {
+                type: 'line',
+                data: chartData,
+                options: chartOptions,
+            });
+        }
+
+        return () => {
+            if (chartInstanceRef.current) {
+                chartInstanceRef.current.destroy();
+            }
+        };
+    }, [salesData]);
 
     return (
         <Box w="100%" h="100vh">
             <Navbar />
             <Box p="20px">
-                {salesChartData.dates.length > 0 ? (
-                    <Line data={chartData} options={chartOptions} key={JSON.stringify(salesData)} />
-                ) : (
-                    <p>Loading chart...</p>
-                )}
+                <canvas id="salesGraph" />
             </Box>
         </Box>
     );
